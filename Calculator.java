5b9d4d5fda9398c5415bebb28b753d4909bed48b
Zeiser <86618798+Zeiser@users.noreply.github.com>
2022-03-28 09:20:25 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,72 @@
+/* -------------------------------------------------------------
+* Author:  Andrew Zeiser
+* Program: Program2
+*
+* Description: 
+* This Program funtions like a calculator. If you create a test 
+* driver you will be able solve any simple arithmetic question. 
+* -------------------------------------------------------------
+*/
+public class Calculator {
+	
+	private double total=0;
+	
+	
+	// Creating the constructors
+	
+	public Calculator() {
+		this(0.0);
+	}
+	
+	public Calculator(double inNumber) {
+		
+		
+		
+		total = inNumber;
+		
+	}
+	
+	
+	// creating the Add Subtract multiply and divide method
+	
+	public double add(double inNumber) {
+		
+		total += inNumber;
+		
+		return total;
+	}
+
+	
+	public double subtract(double inNumber) {
+
+		total-=inNumber;
+		return total;
+	
+	}
+		
+	public double multiply(double inNumber) {
+		total*=inNumber;
+		return total;
+	}
+	
+	public double divide(double inNumber) {
+		total/=inNumber;
+		return total;
+		
+	}
+	
+	// creating the resetFirstNumber method
+	
+	public void resetFirstNumber(double inNumber) {
+		total=inNumber;
+		
+	}
+	
+	public void resetFirstNumber() {
+		total=0.0;
+		
+	}
+	
+	
+	
+}
